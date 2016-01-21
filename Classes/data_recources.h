c2fdfe22886bfe5f7c7a4e9cd75633a6249fdd8c
unknown <>
2016-01-21 15:16:53 +0900

    sujung

--- Classes/data_recources.h
+++ Classes/data_recources.h
@@ -1,7 +1,7 @@
 #pragma once
 /**
 파일 경로 등 변하기 쉬운 변수는 여기에 선언한다
-사용법 : 이 헤더를 인클루드 하고 resData::resources[enum으로 정의한 이름] 
+사용법 : 이 헤더를 인클루드 하고 resData::resources[resData::enum으로 정의한 이름] 
 */
 namespace resData {
 	enum { SPLASH_IMAGE };
