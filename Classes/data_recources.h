1281b7f6e2efdb9cee8c5141ed7c5dcbf599d8de
unknown <>
2016-01-21 15:22:51 +0900

    add Guide

--- Classes/data_recources.h
+++ Classes/data_recources.h
@@ -2,6 +2,7 @@
 /**
 파일 경로 등 변하기 쉬운 변수는 여기에 선언한다
 사용법 : 이 헤더를 인클루드 하고 resData::resources[resData::enum으로 정의한 이름] 
+ex) resData::recources[resData::SPLASH_IMAGE]
 */
 namespace resData {
 	enum { SPLASH_IMAGE };
--- Classes/global.cpp
+++ Classes/global.cpp
@@ -3,6 +3,7 @@
 /*
 전역 변수가 필요하다면 여기를 활용하세요
 새 변수를 만들려면 global.h 도 수정해 주세요
+사용법 ex ) Global::screenSizeY //값을 가져오는 것은 물론 수정도 가능하다
 */
 
 int Global::screenSizeX = 800;
